d2df0040cea8904a20202eabf0bbb90187467bed
Nored <myaminored@gmail.com>
2024-10-29 15:14:15 -0400

    This the first commit

---
+++ Lab01/src/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ * author Myami Nordin
+ * 
+ * the school of Computing
+ * 
+ * I love the school of computing
+ */
+public class Hello {
+	
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World!");		
+		
+		
+	}
+
+}
